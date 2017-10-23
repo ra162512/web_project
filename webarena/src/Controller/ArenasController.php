094fcc458a3c1a537e3566f194cea2b900dcbe8f
Coraline AMEMOUTOU <coraline.amemoutou@edu.ece.fr>
2017-10-23 10:46:09 +0200

    remise en forme du fichier

--- webarena/src/Controller/ArenasController.php
+++ webarena/src/Controller/ArenasController.php
@@ -1,47 +1,49 @@
 <?php
+
 namespace App\Controller;
+
 use App\Controller\AppController;
 use Cake\Event\Event;
 
 /**
-* Personal Controller
-* User personal interface
-*
-*/
-class ArenasController  extends AppController
-{
-    
+ * Personal Controller
+ * User personal interface
+ *
+ */
+class ArenasController extends AppController {
 
+    public function signup() {
+        
+    }
 
+    public function fighter() {
+        
+    }
 
-public function signup(){
- 
-}   
+    public function sight() {
+        
+    }
 
+    public function diary() {
+        
+    }
 
-public function fighter(){}
-public function sight(){}
-public function diary(){}
+    public function accueil() {
+        
+    }
 
-public function accueil(){}
-
-
-
-     public function index()
-     {
-         $this->loadModel('Users');
+    public function index() {
+        $this->loadModel('Users');
         $this->set('users', $this->Users->find('all'));
     }
 
-    public function view($id)
-    {
+    public function view($id) {
         $this->loadModel('Users');
         $user = $this->Users->get($id);
         $this->set(compact('user'));
     }
 
-    public function add() //Pour ajouter un utlisateur
-    {
+    public function add() { //Pour ajouter un utlisateur
         $this->loadModel('Users');
         $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
@@ -55,41 +57,34 @@ public function accueil(){}
         $this->set('user', $user);
     }
 
-   public function beforeFilter(Event $event)  // permet de configurer l'authentification.
-    {
-      
+    public function beforeFilter(Event $event) {  // permet de configurer l'authentification.
+
         parent::beforeFilter($event);
         // Allow users to register and logout.
         // You should not add the "login" action to allow list. Doing so would
         // cause problems with normal functioning of AuthComponent.
-        $this->Auth->allow(['add', 'logout','fighter','diary']); // permet de mettre de faire en sorte que les elements auth laisse publique dans add et logout
+        $this->Auth->allow(['add', 'logout', 'fighter', 'diary']); // permet de mettre de faire en sorte que les elements auth laisse publique dans add et logout
     }
 
-    public function login() //pour se connecter
-    {
-         $this->loadModel('Users');
-     //  $message="vous n'ètes pas co";
-     //   $this->set('message',$message);
-       if ($this->request->is('post')) {    // verifie que la requète utilise bien un post
-          $user = $this->Auth->identify(); //identifie l'utilisateur et renvoie  1 s'il a été identifié
-          
-          if ($user) {  
-       //        $message="Vous êtes bien connecté";
-       //         $this->set('message',$message);
+    public function login() { //pour se connecter
+        $this->loadModel('Users');
+        //  $message="vous n'ètes pas co";
+        //   $this->set('message',$message);
+        if ($this->request->is('post')) {    // verifie que la requète utilise bien un post
+            $user = $this->Auth->identify(); //identifie l'utilisateur et renvoie  1 s'il a été identifié
+
+            if ($user) {
+                //        $message="Vous êtes bien connecté";
+                //         $this->set('message',$message);
                 $this->Auth->setUser($user);    //pour connecter l’utilisateur et sauvegarder les infos de l’utilisateur dans la session 
                 return $this->redirect($this->Auth->redirectUrl());  // redirige vers une url
-                
             }
-           $this->Flash->error(__('Invalid username or password, try again'));
+            $this->Flash->error(__('Invalid username or password, try again'));
         }
     }
- 
-    public function logout()
-    {
+
+    public function logout() {
         return $this->redirect($this->Auth->logout());
     }
 
-    
-
-
-} 
+}
